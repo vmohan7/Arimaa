507057d91579e3371caf95e60813c519a47e1518
VMohan <Vasanth@dn0a2287fb.sunet>
2013-11-02 18:23:02 -0700

    Add stub for Feature Extractor

---
+++ ArimaaBot/src/naive_bayes/FeatureExtractor.java
@@ -0,0 +1,13 @@
+package naive_bayes;
+import arimaa3.*;
+
+public class FeatureExtractor{
+	
+	private GameState prevprev;
+	private GameState prev;
+	
+	public void extract(GameState current_board, ArimaaMove expert_move){
+		
+	}
+	
+}
