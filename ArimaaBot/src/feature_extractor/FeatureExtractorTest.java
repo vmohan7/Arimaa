836945d6b52925dc0682f5d0c25f74c90fddeddb
Neema E <neemazad@gmail.com>
2013-11-10 23:34:07 -0800

    Added our tests to the feature extractor test class (removed from FeatureExtractor)

--- ArimaaBot/src/feature_extractor/FeatureExtractorTest.java
+++ ArimaaBot/src/feature_extractor/FeatureExtractorTest.java
@@ -2,12 +2,16 @@ package feature_extractor;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+import java.util.BitSet;
+
 import org.junit.Test;
 
 import arimaa3.ArimaaMove;
+import arimaa3.Constants;
 import arimaa3.GameState;
 
-public class FeatureExtractorTest {
+public class FeatureExtractorTest implements Constants, FeatureConstants {
 
 	private static String tests[] = {
 		"12w %13 +-----------------+%138|                 |%137|                 |%136|   R             |%135|                 |%134|                 |%133|         D   r   |%132|                 |%131|                 |%13 +-----------------+%13   a b c d e f g h%13",
@@ -25,7 +29,7 @@ public class FeatureExtractorTest {
 	    FeatureExtractor fe = new FeatureExtractor(startState, null);
 	    fe.extractFeatures(new ArimaaMove("Db2n Ha2n Ha3n Hh2n"));
 	    
-	    System.out.println(fe.featureVector.toString());
+//	    System.out.println(fe.featureVector.toString());
 	    
 	    assertTrue(fe.featureVector.get(620));
 	    assertTrue(fe.featureVector.get(68));
@@ -62,5 +66,124 @@ public class FeatureExtractorTest {
 		assertTrue(FeatureExtractor.getLocation(15) == 4);
 		assertTrue(FeatureExtractor.getLocation(57) == 29);
 	}
+	
+	
+	@Test
+	public void testElephantTouchingTraps() {
+		long neighboringTrapSpots = 0;
+		for (int trap = 0; trap < TOUCH_TRAP.length; trap++) 
+			neighboringTrapSpots |= TOUCH_TRAP[trap];
+		
+		GameState gsWhite = new GameState();
+		GameState gsBlack = new GameState();
+		
+		for (int board = 0; board < 64; board++) { //check 64 boards, elephant in different spot each time
+			gsWhite.piece_bb[PT_WHITE_ELEPHANT] = 1L << board;
+			gsBlack.piece_bb[PT_BLACK_ELEPHANT] = 1L << board;
+			
+			boolean whiteEleph = false, blackEleph = false;
+			for (int trap = 0; trap < 4; trap++) {
+				whiteEleph = whiteEleph || TrapExtractor.isElephantTouchingTrap(gsWhite, trap, PL_WHITE);
+				blackEleph = blackEleph || TrapExtractor.isElephantTouchingTrap(gsBlack, trap, PL_BLACK);
+			}
+			
+			boolean onTrap = (neighboringTrapSpots & (1L << board)) != 0;
+			assertTrue(onTrap == whiteEleph);
+			assertTrue(onTrap == blackEleph);
+		}
+	}
+	
+	@Test
+	public void testGetTrapStatus() {
+		// String array containing whiteMove, blackMove pairs (indices 2k, 2k+1)
+		String[] whitesBlacks = {  
+				"Eb3 Rc2", "ed3 rc4", //expect TWO_E == 4, //expect TWO_E == 4
+				"Rf6 Df5 Cf7", "rf6 df5 cf7", //expect TWO_NE == 3, //expect TWO_NE == 3
+				"Ec7 Dd6 Cc5", "rb6" //expect THREE_E == 6, //expect ONE_NE == 1
+		};
+		assertTrue(whitesBlacks.length % 2 == 0);
+		
+		byte[][] whitesBlacksStatuses = {
+				{TrapStatus.TWO_E, TrapStatus.ZERO, TrapStatus.ZERO, TrapStatus.ZERO}, {TrapStatus.TWO_E, TrapStatus.ZERO, TrapStatus.ZERO, TrapStatus.ZERO},
+				{TrapStatus.ZERO, TrapStatus.ZERO, TrapStatus.ZERO, TrapStatus.TWO_NE}, {TrapStatus.ZERO, TrapStatus.ZERO, TrapStatus.ZERO, TrapStatus.TWO_NE},
+				{TrapStatus.ZERO, TrapStatus.THREE_E, TrapStatus.ZERO, TrapStatus.ZERO}, {TrapStatus.ZERO, TrapStatus.ONE_NE, TrapStatus.ZERO, TrapStatus.ZERO}
+		};
+		assertTrue(whitesBlacksStatuses.length == whitesBlacks.length);
+		
+		ArrayList<GameState> gsArr = new ArrayList<GameState>();
+		for (int i = 0; i < whitesBlacks.length / 2; i++)
+			gsArr.add(new GameState(whitesBlacks[i * 2], whitesBlacks[i * 2 + 1]));
+		
+		int game = 0;
+		for (GameState gs : gsArr) {
+			int gameW = game * 2;
+			int gameB = gameW + 1;
+			for (int trap = 0; trap < 4; trap++) {
+				byte wStatus = TrapExtractor.getTrapStatus(gs, trap, PL_WHITE);
+				byte bStatus = TrapExtractor.getTrapStatus(gs, trap, PL_BLACK);
+				
+				assertTrue(wStatus == whitesBlacksStatuses[gameW][trap]);
+				assertTrue(bStatus == whitesBlacksStatuses[gameB][trap]);
+			}
+			game++;
+		}
+	}
+	
+	@Test
+	public void testBitCodes() {
+		BitSet bitset = new BitSet(FeatureRange.TRAP_STATUS_END + 1);
+		
+		String[] whitesBlacks = {  
+				"Eb3 Rc2", "ed3 rc4", //expect TWO_E == 4, //expect TWO_E == 4
+				"Rf6 Df5 Cf7", "rf6 df5 cf7", //expect TWO_NE == 3, //expect TWO_NE == 3
+				"Ec7 Dd6 Cc5", "rb6" //expect THREE_E == 6, //expect ONE_NE == 1
+		};
+		assertTrue(whitesBlacks.length % 2 == 0);
+		
+		int[][] bitIndices = { //each int[] should have 8 bits set, //white, black in each row
+				{/* 8 indices */}, {/* 8 indices */}, //DO MATH for transition from game 1 to game 2
+				{/* 8 indices */}, {/* 8 indices */}  //DO MATH for transition from game 2 to game 3
+		};
+		assertTrue(bitIndices.length == whitesBlacks.length - 2);
+				
+		ArrayList<GameState> gsArr = new ArrayList<GameState>();
+		for (int i = 0; i < whitesBlacks.length / 2; i++)
+			gsArr.add(new GameState(whitesBlacks[i * 2], whitesBlacks[i * 2 + 1]));
+		
+		
+		int bitOffset = FeatureRange.TRAP_STATUS_START;
+		int game = 0;
+		GameState prev = null;
+		for (GameState gs : gsArr) {
+			//if prev is not set, use the first game and start back at the top
+			if (prev == null) {
+				prev = gs;
+				continue;
+			}
+			int gameW = game * 2;
+			int gameB = gameW + 1;
+			
+			TrapExtractor te = new TrapExtractor(prev, gs);
+			te.updateBitSet(bitset);
+			
+			assertTrue(bitset.cardinality() == 8);
+			
+			//check whiteBits
+			for (int bit = 0; bit < bitIndices[gameW].length; bit++) {
+				assertTrue(bitset.get(bitOffset + bitIndices[gameW][bit]));
+			}
+			//check blackBits
+			for (int bit = 0; bit < bitIndices[gameB].length; bit++) {
+				assertTrue(bitset.get(bitOffset + bitIndices[gameB][bit]));
+			}
+			
+			//handle increments / resets
+			bitset.clear();
+			prev = gs;
+			game++;
+		}
+		
+	}
+
 
 }
