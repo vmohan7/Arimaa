440d53e337767f1d4e91cb322a09a428bb0a4e02
Neema E <neemazad@gmail.com>
2013-11-13 22:08:53 -0800

    Finished *but haven't tested* GameParser

--- ArimaaBot/src/naive_bayes/GameParser.java
+++ ArimaaBot/src/naive_bayes/GameParser.java
@@ -2,43 +2,70 @@ package naive_bayes;
 
 import java.util.ArrayList;
 
+import arimaa3.ArimaaMove;
+import arimaa3.GameState;
 import naive_bayes.helper_classes.*;
 
 public class GameParser {
 	
 	private ArimaaState arimaaState;
-	String[] moveList;
+	private String[] moveList;
+	private int count;
 	
 	//TODO: check if "takeback" is handled as a move...and then handle it
 	
 	public GameParser(GameInfo myGameInfo) {
-		ArrayList<String> tempMoveList = new ArrayList<String>();
-		moveList = myGameInfo.getMoveList().split("\\r?\\n"); //split on newlines
-		
-		for (int move = 0; move < moveList.length; move++) {
-			if (moveList[move])
-		}
+		initMoveList(myGameInfo);
+		initArimaaState(myGameInfo);
+		count = 0;
 	}
 	
-	
 	public boolean hasNextGameState() {
-		
-		
-		return false;
+		return count < moveList.length;
 	}
 	
 	/**
-	 *  @return An <b>ArimaaState</b> containing 4 fields:
-	 *    <br>0 - previous previous board
+	 *  @return An <b>ArimaaState</b> containing 3 fields:
 	 *    <br>1 - previous board
-	 *    <br>2 - the move itself
-	 *    <br>3 - current board
+	 *    <br>2 - current board, resulting from the move applied to previous board
+	 *    <br>3 - the move itself
 	 *    <br><i> or </i><b>null</b><i> if there are no more game states 
 	 *    			(i.e. the game is over)</i>
 	 */
 	public ArimaaState getNextGameState() {
-		//return null if no more game states
+		if (!hasNextGameState()) return null;
 		
-		return null;
+		ArimaaState tempAS = new ArimaaState(arimaaState);
+		arimaaState.update(new ArimaaMove(moveList[count]));
+		return tempAS;
 	}
+	
+	
+	private void initMoveList(GameInfo gi) {
+		ArrayList<String> tempMoveList = new ArrayList<String>();
+		moveList = gi.getMoveList().split("\\r?\\n"); //split on newlines
+		
+		for (int move = 0; move < moveList.length; move++) {
+			if (moveList[move].indexOf("takeback") != -1)
+				tempMoveList.add(moveList[move]);
+			else 
+				/* if the move is "takeback", that means the previous move was 
+				 * replaced with the one about to be considered */
+				tempMoveList.remove(tempMoveList.size() - 1);
+		}
+		
+		//remove the very last "move", which is generally an empty move signifying end of game
+		tempMoveList.remove(tempMoveList.size() - 1);
+		
+		moveList = (String[])tempMoveList.toArray();
+	}
+	
+	private void initArimaaState(GameInfo gi) {
+		String white = gi.getWhiteStartState();
+		String black = gi.getBlackStartState();
+		
+		GameState gs = new GameState(white, black);
+		arimaaState = new ArimaaState(gs); //no previous games, no moves
+	}
+	
 }
--- ArimaaBot/src/naive_bayes/helper_classes/ArimaaState.java
+++ ArimaaBot/src/naive_bayes/helper_classes/ArimaaState.java
@@ -4,24 +4,45 @@ import arimaa3.ArimaaMove;
 import arimaa3.GameState;
 
 public class ArimaaState {
-	private GameState prev_prev, prev, curr;
+	private GameState prev, curr;
 	private ArimaaMove move;
 	
 	/** 
-	 * <b>A struct with 4 fields:</b> 
-	 *  <br>1. previous previous game state, 
-	 *  <br>2. previous game state,
-	 *  <br>3. current game state,
-	 *  <br>4. the move from previous to current
+	 * <b>A struct with 3 fields:</b> 
+	 *  <br>1. previous game state,
+	 *  <br>2. current game state,
+	 *  <br>3. the move from previous to current
 	 * */
-	public ArimaaState(GameState prev_prev, GameState prev, GameState curr, ArimaaMove move) {
-		this.prev_prev = prev_prev;
+	public ArimaaState(GameState prev, GameState curr, ArimaaMove move) {
 		this.prev = prev;
 		this.curr = curr;
 		this.move = move;
 	}
+	
+	/** Copy constructor for an ArimaaState */
+	public ArimaaState(ArimaaState toCopy) {
+		this.prev = toCopy.getPrev();
+		this.curr = toCopy.getCurr();
+		this.move = toCopy.getMove();
+	}
+	
+	/** Constructor for initializing an ArimaaState at the beginning of a game. */
+	public ArimaaState(GameState startOfGame) {
+		this(null, startOfGame, null);
+	}
+	
+	/** 
+	 * Updates the fields of an ArimaaState given a move to be played.
+	 * */
+	public void update(ArimaaMove nextMove) {
+		prev = curr;
+		
+		GameState next = new GameState();
+		next.playFullClear(nextMove, curr);
+		curr = next;
+		move = nextMove;
+	}
 
-	public GameState getPrev_prev() { return prev_prev; }
 	public GameState getPrev() { return prev; }
 	public GameState getCurr() { return curr; }
 	public ArimaaMove getMove() { return move; }
