294c9afa4a44fec21565e7523c34ca4ab29da6bc
Vivek Choksi <viv.choksi@gmail.com>
2013-11-26 15:33:10 -0800

    Removed 500MB java command line argument.

--- Testing/arimaa_nb_corn.py
+++ Testing/arimaa_nb_corn.py
@@ -15,10 +15,10 @@ PASSWORD = passwords.my_password
 NUM_TRIALS = 1
 
 # Number of examples in first test
-START_SIZE = 2
+START_SIZE = 150
 
 # Number of examples in last test (inclusive)
-END_SIZE = 4
+END_SIZE = 150
 
 # Increment by which to increase number of examples in successive tests
 INCR_SIZE = 1
