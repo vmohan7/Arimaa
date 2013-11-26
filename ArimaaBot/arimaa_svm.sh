a6373a0cde752b452f1849538271fc4cb2a63a85
VMohan <vmohan7@stanford.edu>
2013-11-25 21:56:22 -0800

    Testing

--- ArimaaBot/arimaa_svm.sh
+++ ArimaaBot/arimaa_svm.sh
@@ -7,8 +7,8 @@
 #$ -cwd
 
 #arguments is 1= the number of games; 2 = the model
-TRAIN=$(echo '.7 * $1' | bc | sed 's/[.].*//')
-TEST=$(echo '.3 * $1' | bc | sed 's/[.].*//')
+TRAIN=$(echo '.7 * ' $1 | bc | sed 's/[.].*//')
+TEST=$(echo '.3 * ' $1 | bc | sed 's/[.].*//')
 java -Xmx500000000 -classpath ./mysql-connector-java-5.1.26-bin.jar:./bin:./ svm.SVMMain --train train$1.txt $TRAIN gameIDS$1.dat
 ./liblinear-1.94/svm-train -s $2 train$1.txt model_$2_for_$1.model
 java -Xmx500000000 -classpath ./mysql-connector-java-5.1.26-bin.jar:./bin:./ svm.SVMMain --test model_$2_for_$1.model $TEST gameIDS$1.dat
