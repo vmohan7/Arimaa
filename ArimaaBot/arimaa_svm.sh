4810815064d3c85b04bd5bd3792f6f5d379b3707
VMohan <vmohan7@stanford.edu>
2013-11-25 21:31:10 -0800

    Add svm library and barley scripts

---
+++ ArimaaBot/arimaa_svm.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# tell grid engine to merge stdout and stderr streams
+#$ -j y
+
+# tell grid engine to use current directory
+#$ -cwd
+
+#arguments is 1= the number of games; 2 = the model
+TRAIN=echo '.7 * $1' | bc | sed 's/[.].*//'
+TEST=echo '.3 * $1' | bc | sed 's/[.].*//'
+java -Xmx500000000 -classpath ./mysql-connector-java-5.1.26-bin.jar:./bin:./ svm.SVMMain --train train$1.txt $TRAIN gameIDS$1.dat
+./svm-train -s $2 train$1.txt model_$2_for_$1.model
+java -Xmx500000000 -classpath ./mysql-connector-java-5.1.26-bin.jar:./bin:./ svm.SVMMain --test model_$2_for_$1.model $TEST gameIDS$1.dat
