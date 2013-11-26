e81253adad96d8c7dfe14cdf736963e3fc1bc384
Vivek Choksi <viv.choksi@gmail.com>
2013-11-26 02:12:07 -0800

    updated instructions; parallelizing on corn is ready to use now.

--- Testing/arimaa_nb_barley.sh
+++ Testing/arimaa_nb_barley.sh
@@ -2,7 +2,7 @@
 
 
 # VARIABLE TO CONFIGURE: jar path relative to the Arimaa directory
-JAR_PATH=./ArimaaBot/mysql-connector-java-5.1.26-bin.jar
+JAR_PATH=../ArimaaBot/mysql-connector-java-5.1.26-bin.jar
 
 # tell grid engine to merge stdout and stderr streams
 #$ -j y
@@ -18,5 +18,5 @@ JAR_PATH=./ArimaaBot/mysql-connector-java-5.1.26-bin.jar
 # run the java program with the same args as the args to this script
 # this assumes that we're running from the root Arimaa directory
 
-# Configure the location and name of the mysql-connector jar file in this line!!
-java -Xmx500000000 -classpath $JAR_PATH:./ArimaaBot/bin:./ArimaaBot naive_bayes.NBMain $1 $2 $3 
+# Configure the paths in this line if your directory structure is different! 
+java -Xmx500000000 -classpath $JAR_PATH:../ArimaaBot/bin:../ArimaaBot naive_bayes.NBMain $1 $2 $3 
