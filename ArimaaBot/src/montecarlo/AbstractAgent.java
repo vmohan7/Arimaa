96e37adee7e5769b123fd3f175060f1e83682dbb
VMohan <vmohan7@stanford.edu>
2013-11-27 17:13:13 -0800

    Increase random chance

--- ArimaaBot/src/montecarlo/AbstractAgent.java
+++ ArimaaBot/src/montecarlo/AbstractAgent.java
@@ -9,15 +9,13 @@ import utilities.helper_classes.ArimaaState;
 public abstract class AbstractAgent {
 	
 	protected boolean training;
-	protected int time;
 	protected double[] weights;
 	
-	public static final double RANDOM_CHOICE = 0.15;
+	public static final double RANDOM_CHOICE = 0.20;
 	
 	public AbstractAgent(double[] weights, boolean training){
 		setWeights(weights);
 		this.training = training; 
-		time = 0;
 	}
 	
 	/**
@@ -38,7 +36,6 @@ public abstract class AbstractAgent {
 		if (training){
 			Random r = new Random();
 			if( r.nextDouble() < RANDOM_CHOICE ){ 
-				time++; //allows for random to be less of an issue as we have train more
 				return movelist[ r.nextInt( movelist.length ) ];
 			}
 		}
