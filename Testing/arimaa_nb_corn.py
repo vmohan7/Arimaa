03585643a7ce7f5cd87ffaad8d6bbc4a4026793f
Vivek Choksi <viv.choksi@gmail.com>
2013-12-01 02:21:02 -0800

    updated corn parallelization code -- exiting from expect.

--- Testing/arimaa_nb_corn.py
+++ Testing/arimaa_nb_corn.py
@@ -16,23 +16,17 @@ PASSWORD = passwords.my_password
 NUM_TRIALS = 4
 
 # Number of examples in first test
-START_SIZE = 10
+START_SIZE = 120
 
 # Number of examples in last test (inclusive)
-END_SIZE = 50
+END_SIZE = 150
 
 # Increment by which to increase number of examples in successive tests
 INCR_SIZE = 10
 
 # Keeps track of which corn machine to use
-counter = 0
+counter = 5
 
-for exampleSetSize in range(START_SIZE, END_SIZE + 1, INCR_SIZE):
-    for trialIndex in range(NUM_TRIALS):
-        os.system("/usr/bin/expect -d -f arimaa_nb_corn.exp %d %d %d %s '%s' '%s' >> NBcorn10-50.txt &" 
-            % (exampleSetSize, exampleSetSize, 1, USERNAME, PASSWORD, getCornMachine(counter)))
-        counter = counter + 1
-        # time.sleep(10)
 
 # There are 30 corn machines. This function returns a string corresponding 
 # to the 'next' corn machine. e.g. '01'
@@ -42,3 +36,12 @@ def getCornMachine(a):
         return '0' + str(a)
     else: 
         return str(a)
+
+
+for exampleSetSize in range(START_SIZE, END_SIZE + 1, INCR_SIZE):
+    for trialIndex in range(NUM_TRIALS):
+        os.system("/usr/bin/expect -d -f arimaa_nb_corn.exp %d %d %d %s '%s' '%s' >> NBcorn120-160.txt &" 
+            % (exampleSetSize, exampleSetSize, 1, USERNAME, PASSWORD, getCornMachine(counter)))
+        counter = counter + 1
+        # time.sleep(10)
+
