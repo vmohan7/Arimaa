9e583166648de0086e5f02e8618a21bcea2c7594
Vivek Choksi <viv.choksi@gmail.com>
2013-12-10 01:03:57 -0800

    Added script to plot cdf of percentiles.

--- Plotting/create_plots.m
+++ Plotting/create_plots.m
@@ -1,2 +1,6 @@
-plot_learning_curves('learn.csv');
-plot_percentile_histogram('../Testing/nb_hists.csv');
+
+ymax = 0.3;
+plot_percentile_histogram('../Testing/nb_hists.csv', ymax);
+
+% plot_learning_curves('learn.csv');
+plot_percentile_cdf('../Testing/nb_hists.csv');
--- Plotting/plot_percentile_histogram.m
+++ Plotting/plot_percentile_histogram.m
@@ -1,4 +1,4 @@
-function M = plot_percentile_histogram(csvname)
+function h = plot_percentile_histogram(csvname, ymax)
 
 % Read data for the percentile of each expert move from a .csv file 
 % and create a histogram.
@@ -6,16 +6,15 @@ function M = plot_percentile_histogram(csvname)
 % M is a column vector of percentiles of all expert moves evaluated
 M = csvread(csvname);
 
-%% Plot "percentile or expert move" learning curve
-
 figure('Color',[1.0 1.0 1.0]);
 nBins = 100;
 
 [n,x] = hist(M, nBins);
 bar(x, n./sum(n),1,'hist'); % normalize y axis to give proportions instead of counts
-axis([0 1 0 0.3]) % gives x min/max and y min/max for axis scaling: [xmin xmax ymin ymax]
+h = gca; % get a handle to the current axis
+axis([0 1 0 ymax]) % gives x min/max and y min/max for axis scaling: [xmin xmax ymin ymax]
 
 
 title('Percentile rankings of expert moves', 'FontSize', 20);
 xlabel('Percentile of expert move among all ordered moves', 'FontSize', 16);
-ylabel('Count', 'FontSize', 16);
+ylabel('Proportion', 'FontSize', 16);
