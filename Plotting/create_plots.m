4e7a4b2e032b29367d1de8fd19051ead5875c07f
Vivek Choksi <viv.choksi@gmail.com>
2013-12-10 01:14:16 -0800

    added a few comments

--- Plotting/create_plots.m
+++ Plotting/create_plots.m
@@ -1,5 +1,5 @@
 
-ymax = 0.3;
+ymax = 0.3; % The max value of the y axis for the histogram
 plot_percentile_histogram('../Testing/nb_hists.csv', ymax);
 
 % plot_learning_curves('learn.csv');
--- Plotting/plot_percentile_cdf.m
+++ Plotting/plot_percentile_cdf.m
@@ -6,7 +6,7 @@ function M = plot_percentile_cdf(csvname)
 % M is a column vector of percentiles of all expert moves evaluated
 M = csvread(csvname);
 
-M = 1.0 - M;
+M = 1.0 - M; % Since we want "proportion in top n percent"
 
 figure('Color',[1.0 1.0 1.0]);
 
@@ -19,6 +19,6 @@ title('Proportion of expert moves in top n percent', 'FontSize', 20);
 xlabel('n', 'FontSize', 16);
 ylabel('Proportion', 'FontSize', 16);
 
-% Change x axis to display percents
+% Change x axis to display percents (e.g. 20% instead of 0.2)
 x_labels=100*str2num(get(gca,'XTickLabel'));
 set(gca, 'XTickLabel', sprintf('%d%%|', x_labels))
