da85e2100e8077ae9b27ff3d562b845e0ddea710
Vivek Choksi <viv.choksi@gmail.com>
2013-12-10 22:06:13 -0800

    Added comments

--- Plotting/plot_learning_curves.m
+++ Plotting/plot_learning_curves.m
@@ -20,27 +20,30 @@ levelOfPerformanceX = linspace(xStartTick, xEndTick, linspaceSteps);
 %% Plot "percentile of expert move" learning curve
 
 % Set figure color and position
-figure('Color',[1.0 1.0 1.0],'Position', [100, 100, 750, 550]);
+figure('Color',[1.0 1.0 1.0]);
+% figure('Position', [100, 100, 750, 550]);
 
-% plot(M(:,1), M(:,[2 3]), 'LineWidth', 1.15, 'LineSmoothing','on');
+% Plot learning curve
 plot(M(:,1), M(:,[2 3]), 'LineWidth', 1);
-
-plotTitle = strcat(modelName, ' learning curve for expert-move percentiles');
-title(plotTitle, 'FontSize', 20);
-xlabel('Number of training examples', 'FontSize', 16);
-set(gca, 'XTick', xStartTick:xStep:xEndTick); % set the x-tick marks according to input parameters
-ylabel('Percentile of expert move among all ordered moves', 'FontSize', 16);
-axis([0 xEndTick yMin yMax]) % gives x min/max and y min/max for axis scaling: [xmin xmax ymin ymax]
-
-
 hold on;
 
-% Draw dotted line showing convergence
-convVal=mean([M(end,2) M(end,3)]);
+% Draw dashed line showing convergence
+convVal=mean([M(end,2) M(end,3)]); % approximation for convergence percentile value
 convLineX = [min(M(:,1)) max(M(:,1))];
 convLineY = [convVal convVal];
 plot(convLineX,convLineY,'--k');
 
+% Titles and label
+plotTitle = strcat(modelName, ' learning curve for expert-move percentiles');
+title(plotTitle, 'FontSize', 20);
+xlabel('Number of training examples', 'FontSize', 16);
+ylabel('Percentile of expert move among all ordered moves', 'FontSize', 16);
+
+% Configure axes ticks and range
+set(gca, 'XTick', xStartTick:xStep:xEndTick); % set the x-tick marks according to input parameters
+axis([0 xEndTick yMin yMax]) % gives x min/max and y min/max for axis scaling: [xmin xmax ymin ymax]
+
+% Configure legend
 myLegend = legend('Training set', 'Testing set', 'Approximate convergence');
 set(myLegend,'Location','SouthEast');
 set(myLegend, 'Fontsize', 16);
@@ -49,15 +52,15 @@ hold off;
 
 
 %% Plot "proportion in top 5%" learning curve
-figure('Color',[1.0 1.0 1.0]);
-
-plot(M(:,1), M(:,[4 5]), levelOfPerformanceX, levelOfPerformanceY, 'LineWidth', 1);
-
-plotTitle = strcat(modelName, ' learning curve for proportion of expert moves in top 5%');
-title(plotTitle, 'FontSize', 20);
-xlabel('Number of training examples', 'FontSize', 16);
-set(gca, 'XTick', xStartTick:xStep:xEndTick); % set the x-tick marks according to input parameters
-ylabel('Proportion of expert moves ranked in top 5% of moves', 'FontSize', 16);
-myLegend = legend('Training set', 'Testing set', 'Desired performance');
-set(myLegend,'Location','SouthEast');
-set(myLegend, 'Fontsize', 16);
+% figure('Color',[1.0 1.0 1.0]);
+% 
+% plot(M(:,1), M(:,[4 5]), levelOfPerformanceX, levelOfPerformanceY, 'LineWidth', 1);
+% 
+% plotTitle = strcat(modelName, ' learning curve for proportion of expert moves in top 5%');
+% title(plotTitle, 'FontSize', 20);
+% xlabel('Number of training examples', 'FontSize', 16);
+% set(gca, 'XTick', xStartTick:xStep:xEndTick); % set the x-tick marks according to input parameters
+% ylabel('Proportion of expert moves ranked in top 5% of moves', 'FontSize', 16);
+% myLegend = legend('Training set', 'Testing set', 'Desired performance');
+% set(myLegend,'Location','SouthEast');
+% set(myLegend, 'Fontsize', 16);
