a01c21a8cc06d22ede048b54be7ab6cabd648f9f
VMohan <vmohan7@stanford.edu>
2014-01-23 23:40:57 -0800

    Constants

--- ArimaaBot/src/game_phase/GamePhaseClusterer.java
+++ ArimaaBot/src/game_phase/GamePhaseClusterer.java
@@ -8,9 +8,13 @@ import utilities.helper_classes.GameInfo;
 import utilities.helper_classes.Utilities;
 
 public class GamePhaseClusterer {
+	
+	public static final int NUM_GAMES = 5;
+	public static final int CLUSTERS = 3;
+	public static final int ITERATIONS = 100;
 
 	public static void main(String args[]){
-		GameData myGameData = new GameData(1000, 1.0);
+		GameData myGameData = new GameData(NUM_GAMES, 0.99);
 		Utilities.printInfo("Finished fetching game data");
 		ArrayList<double[]> trainMatrix = new ArrayList<double[]>();
 		
@@ -31,14 +35,16 @@ public class GamePhaseClusterer {
 		}
 		
 		double[][] matrix = trainMatrix.toArray(new double[1][1]);
-		KMeansWrapper kmeans = new KMeansWrapper(3, 1000, matrix);
+		KMeansWrapper kmeans = new KMeansWrapper(CLUSTERS, ITERATIONS, matrix);
 		kmeans.cluster();
 		double[][] centers = kmeans.centroids();
 		
-		for(int i = 0; i < centers.length; i++){
-			for(int j = 0; j < centers[i].length; j++){
-				//Print out centroids
+		for(int j = 0; j < centers[0].length; j++){
+			System.out.print(j + "\t");
+			for(int i = 0; i < centers.length; i++){
+				System.out.print(i + " " );
 			}
+			System.out.println("");
 		}
 	}
 
