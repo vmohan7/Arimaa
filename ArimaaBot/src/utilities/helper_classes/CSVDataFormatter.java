a86b739b80abf8223a592da100b88df82be75c1f
Arzav <arzavj@stanford.edu>
2014-02-01 18:17:08 -0800

    data points can now be assigned to clusters and csv's are outputted

--- ArimaaBot/src/utilities/helper_classes/CSVDataFormatter.java
+++ ArimaaBot/src/utilities/helper_classes/CSVDataFormatter.java
@@ -1,7 +1,11 @@
 package utilities.helper_classes;
 
+import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.FileWriter;
+import java.io.IOException;
 import java.io.PrintWriter;
 
 /** A generic CSV writer, with convenience methods! */
@@ -43,11 +47,54 @@ public class CSVDataFormatter {
 			PrintWriter pw = new PrintWriter(filename);
 			pw.print(sb.toString());
 			pw.close();
+			Utilities.printInfo("File ouput to " + (new File(filename)).getAbsolutePath());
 			return true;
 		} catch (FileNotFoundException e) {
 			System.err.println("Invalid filename: " + filename);
 			e.printStackTrace();
 			return false;
 		}	
+	}
+	
+	//to test functionality
+	public static void main(String args[]) {
+		final String filename = "CSVTester.csv";
+		CSVDataFormatter csvDF = new CSVDataFormatter();
+		
+		String[] values = {"1", "2", "3", "4"};
+		for (int i = 0; i < values.length; i++)
+			csvDF.appendValue(values[i]);
+		csvDF.nextLine();
+		for (int i = 0; i < values.length - 1; i++)
+			csvDF.appendValue(values[i]);
+		csvDF.nextLine();
+		for (int i = values.length - 1; i >= 0; i--)
+			csvDF.appendValue(values[i]);
+		
+		if (!csvDF.finalizeFile(filename)) return;
+		
+		File f = new File(filename);
+		Utilities.printInfo("File absolute path: " + f.getAbsolutePath());
+		
+		Utilities.printInfo("File validation result: " + validateFile(filename));
+	}
+	
+	public static boolean validateFile(String filename) {
+		BufferedReader rd;
+		try {
+			FileReader f;
+			rd = new BufferedReader(f = new FileReader(filename));
+			while (true) {
+				String line = rd.readLine();
+				if (line == null) break;
+				Utilities.printInfo(line);
+			}
+			rd.close();
+			return true;
+		}
+		catch (IOException e) {
+			return false;
+		}
+		
 	}
 }
