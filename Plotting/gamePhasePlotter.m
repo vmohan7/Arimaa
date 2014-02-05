19ea4f29fbce54c45a83727152ff44f866a4dc25
Arzav <arzavj@stanford.edu>
2014-02-04 23:03:49 -0800

    added plotting code for game phase

---
+++ Plotting/gamePhasePlotter.m
@@ -0,0 +1,25 @@
+fid = fopen('kmeans_3.csv');
+
+% ignore the first this many columns in the csv (perhaps if the first
+% column is the game id)
+columnOffset = 1;
+
+tline = fgetl(fid);
+colors = ['k', 'b', 'g', 'r'];
+color = 1;
+
+while ischar(tline)
+    vec = str2num(tline);
+    titleAddition = '';
+    
+    if (columnOffset ~= 0)
+        titleAddition = num2str(vec(1:columnOffset));
+    end;
+    
+    plotGamePhase(vec(1+columnOffset:end), titleAddition, colors(color));
+    tline = fgetl(fid);
+    color = mod(color, numel(colors)) + 1;
+end
+
+fclose(fid);
+disp('Type close all to programatically close all figures');
