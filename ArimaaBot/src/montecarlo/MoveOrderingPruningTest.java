235e51c69cad9fda0786da271d008dda64a372d3
Arzav <arzavj@stanford.edu>
2014-02-13 13:10:07 -0800

    renamed file agents and renamed some basic variables

---
+++ ArimaaBot/src/montecarlo/MoveOrderingPruningTest.java
@@ -0,0 +1,25 @@
+package montecarlo;
+
+import static org.junit.Assert.*;
+
+import java.util.PriorityQueue;
+
+import montecarlo.MoveOrderingPruning.MoveOrder;
+
+import org.junit.Test;
+
+public class MoveOrderingPruningTest {
+
+	@Test
+	public void test() {
+		NaiveReflexAgent temp = new NaiveReflexAgent(null, false, null);
+		PriorityQueue<MoveOrder> minPQ = new PriorityQueue<MoveOrder>( 3, temp.new MoveOrder(true) );
+		minPQ.add(temp.new MoveOrder(null, 0.5) );
+		minPQ.add(temp.new MoveOrder(null, 0.6) );
+		
+		assertEquals(minPQ.remove(), 0.5);
+		assertEquals(minPQ.remove(), 0.6);
+
+	}
+
+}
