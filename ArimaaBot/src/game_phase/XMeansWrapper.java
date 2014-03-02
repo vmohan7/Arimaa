2f2ad93a701bfbe6682f1eca042093c6a1279f90
NZadeh <neemazad@gmail.com>
2014-03-01 17:35:17 -0800

    Started adding things that look reasonable--documentation seems inconsistent. Need to figure out how to use library. Maybe revert to older version for which documentation is present?

--- ArimaaBot/src/game_phase/XMeansWrapper.java
+++ ArimaaBot/src/game_phase/XMeansWrapper.java
@@ -1,8 +1,14 @@
 package game_phase;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 import weka.clusterers.XMeans;
+import weka.core.FastVector;
+import weka.core.Instance;
+import weka.core.DenseInstance;
+import weka.core.Instances;
+import weka.core.Attribute;
 
 /** 
  * This class extends XMeans.
@@ -15,13 +21,28 @@ public class XMeansWrapper extends XMeans {
 	/**
 	 * TODO: What is this auto-generated code?
 	 */
-	
 	private static final long serialVersionUID = 3790499446630073185L;
 	// END TODO
 	
 	public static void main(String[] args) {
-		XMeans asdf = new XMeans();
-		asdf.getTechnicalInformation();
+		double[][] designMatrix = { {0, 1, 2}, {1, 0, 2}, {0, -1, -2}, {-1, 0, -2} };
+		XMeansWrapper xmw = new XMeansWrapper(3, 5, designMatrix);
+		
+		//TODO: Change this...
+		final String NAME = "What?";
+		final FastVector ATT_INFO = new FastVector();
+		final int CAPACITY = designMatrix.length;
+		
+		//TODO http://stackoverflow.com/questions/12118132/adding-a-new-instance-in-weka
+		//TODO Documentation seems inconsistent...
+		Instances dataset = new Instances(NAME, ATT_INFO, CAPACITY);
+		for (int i = 0; i < designMatrix.length; i++){
+			Instance datapoint = null; // Documentation seems inconsistent...
+			dataset.add(datapoint);
+		}
+		
+		xmw.buildClusterer(dataset);
+		//TODO SO SAD :(
 	}
 	
 	
