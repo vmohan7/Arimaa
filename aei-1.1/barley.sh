dc5815c202633862a7f87bf4c7ad000553f9e927
Vasanth Mohan <vmohan7@corn03.stanford.edu>
2014-03-04 22:51:20 -0800

    Change with settings

--- aei-1.1/barley.sh
+++ aei-1.1/barley.sh
@@ -1,1 +1,14 @@
+#!/bin/bash
+
+# tell grid engine to merge stdout and stderr streams
+#$ -j y
+
+# mail to this address
+#$ -M vmohan7@stanford.edu
+# send mail on beginning, ending, or suspension of job
+#$ -m bes
+
+# tell grid engine to use current directory
+#$ -cwd
+
 python loop_games.py
