7e1707f03fd0854307f9ed1c377c0fdb26fb1e14
NZadeh <neemazad@gmail.com>
2014-03-05 21:59:40 -0800

    Modified MultiNBTest a bit... :D

--- ArimaaBot/src/naive_bayes/MultiNBTest.java
+++ ArimaaBot/src/naive_bayes/MultiNBTest.java
@@ -1,27 +1,18 @@
 package naive_bayes;
 
 import static org.junit.Assert.*;
+import game_phase.XMeansWrapper;
 
 import org.junit.Test;
 
-import weka.core.Instances;
 import arimaa3.Constants;
 
 public class MultiNBTest implements Constants {
 	
-	private static final double[][] designMatrix = { 
-			{2, 1}, {2, 2}, {2, 3}, {1, 2}, {3, 2}, // quadrant 1
-			{-2, 1}, {-2, 2}, {-2, 3}, {-1, 2}, {-3, 2}, // quadrant 2
-			{-2, -1}, {-2, -2}, {-2, -3}, {-1, -2}, {-3, -2}, // quadrant 3
-			{2, -1}, {2, -2}, {2, -3}, {1, -2}, {3, -2}, // quadrant 4
-	};
-	
-	private static Instances clusters = null;
-	private static int[] clusterAssignments = new int[designMatrix.length];
-	
-	
+
 	@Test
 	public void serializationTest() {
+		XMeansWrapper.main(null); // <-- can comment this line out if XMeans.ser is updated locally
 		trainAndWriteToFile();
 		deserializeAndTest();
 	}
