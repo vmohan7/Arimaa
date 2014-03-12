5033075320f2db57d0ff794d6f73be5a78f6c8b1
NZadeh <neemazad@gmail.com>
2014-03-11 23:34:05 -0700

    Tried to improve the MultiNBMain constants... SO IMPOSSIBLE.

--- ArimaaBot/src/naive_bayes/MultiNBMain.java
+++ ArimaaBot/src/naive_bayes/MultiNBMain.java
@@ -7,65 +7,131 @@ import utilities.helper_classes.Utilities;
 
 public class MultiNBMain {
 
+	private static final boolean DO_NOT_TEST = false, TEST = true;
+	private static final int IGNORED = -1; 			// value is unimportant -- used for the English
+	private static final boolean IGNORED_B = false; // value is unimportant -- _B is for boolean
+	
 	
 	/* ================ VALUES TO CONFIGURE ================ */
 
+	/* Fractions of GameData games used for training the model. */
 	/** Used in trainAndTest if testing == false. */
 	private static final double TRAIN_FRACTION = 0.7;
 	/** Used in trainAndTest if testing == true. */
 	private static final double ALMOST_ALL = 0.999999;
 	
-	/* If set to false, then log statements will be printed. If set to true, then only 
-	 * results will be reported, in a csv-importable format. */
-	public static final boolean PARSEABLE_OUTPUT = true;
-
-	/* If set to true, then the percentile of each expert move evaluated will be printed
-	 * in a csv-importable format. */
-	public static final boolean PRINT_PERCENTILES = true;
-	
-	private static final boolean TEST_TRAINING_SET_SIZES = true;
-	private static final int NUM_GAMES = 151;
-	
-	private static final boolean DO_NOT_TEST = false, TEST = true;
-	
+	/** Rating of the games in the GameData. */
 	private static final int RATING = 2100;
 	
-	private static final boolean RUN_FROM_COMMAND_LINE = false;
+	/** 
+	 * If set to false, then log statements will be printed. If set to true, then only 
+	 * results will be reported, in a csv-importable format. 
+	 */
+	public static final boolean PARSEABLE_OUTPUT = true;
+
+	/** 
+	 * If set to true, then the percentile of each expert move evaluated will be printed
+	 * in a csv-importable format. 
+	 */
+	public static final boolean PRINT_PERCENTILES = true;
+	
+	/** Run testing on a range of game-set sizes. */
+	private static final boolean TEST_TRAINING_SET_SIZES = true;
+	
 	
 	/* These values are used if RUN_FROM_COMMAND_LINE is false. They specify the size
 	 * of the example set for the start round and the end round, and the amount by which
 	 * to increment the size of the example set. E.g. if {START_SIZE, END_SIZE, INCREMENT} 
 	 * == {10, 50, 10}, then the program will train on example sets of size 10, 20, ... 50. 
 	 */
-	private static final int START_SIZE = 10;
-	private static final int END_SIZE = 10;
-	private static final int INCREMENT = 10;
+	/** Values to configure the set: { x = START_SIZE + k*INCREMENT | START_SIZE <= x <= END_SIZE, k natural } */
+	private static final int START_SIZE = 	TEST_TRAINING_SET_SIZES ? 10 : IGNORED;
+	private static final int END_SIZE = 	TEST_TRAINING_SET_SIZES ? 10 : IGNORED;
+	private static final int INCREMENT = 	TEST_TRAINING_SET_SIZES ? 10 : IGNORED;
+	
+	/** Set to true if you want a serialized model with END_SIZE games. */
+	private static final boolean SERIALIZE_AFTER_TEST = TEST_TRAINING_SET_SIZES ? false : IGNORED_B;
+	
+	/** Running from the command line only matters if testing. */
+	private static final boolean RUN_FROM_COMMAND_LINE = TEST_TRAINING_SET_SIZES ? false : IGNORED_B;
+	
+	/** 
+	 * This variable is used when training only (and not testing). 
+	 * ALMOST_ALL * NUM_GAMES games will be trained on. 
+	 */
+	private static final int NUM_GAMES = TEST_TRAINING_SET_SIZES ? IGNORED : 151;
 	
 	/* ===================================================== */
 	
-	// Instructions to redirect console output to file:
-	// right click MultiNBMain.java -> Run as -> Run Configurations... -> select "Common" tab
-	// 			 				    -> check "File" under "Standard Input and Output"
-	//      	                    -> enter destination file name :D
+	
+	/* Instructions to redirect console output to file:
+	 * right click MultiNBMain.java -> Run as -> Run Configurations... -> select "Common" tab
+	 * 		 				    	-> check "File" under "Standard Input and Output"
+	 *								-> enter destination file name :D  
+	 */
 	/** 
-	 * Outputs a MultiNBHypothesis object to a file for later deserialization and use.
-	 * @param args Optional command line arguments (for testing)
+	 * Main has two options: <br>
+	 *  1. Trains and tests a MultiNBHypothesis model on different game-set sizes. <br>
+	 *  2. Trains a MultiNBHypothesis model and outputs a serialized file.
+	 * @param args Optional command line arguments (only potentially used with option 1: testing)
 	 */
 	public static void main(String[] args) {
-		
 		if (TEST_TRAINING_SET_SIZES) {
-			final boolean serializeAfterTest = false;
-			testDifferentTrainingSetSizes(args, serializeAfterTest);
+			testDifferentTrainingSetSizes(args, SERIALIZE_AFTER_TEST);
 		}
 		else {
-			// The MultiNBHypothesis contains all of the trained parameters...
 			MultiNBHypothesis mnbh = trainAndTest(NUM_GAMES, DO_NOT_TEST);
 			mnbh.serialize();
 		}
+	}
+	
+	
+	// ===========================================================================
+	
+		
+	private static void testDifferentTrainingSetSizes(String[] args) {
+		testDifferentTrainingSetSizes(args, false);
+	}
+	
+	private static void testDifferentTrainingSetSizes(String[] args, boolean serialize) {
+		final long totalStartTime = System.currentTimeMillis();
+		
+		if (RUN_FROM_COMMAND_LINE && args.length != 3){
+			System.out.println("Error in MultiNBMain: expects 3 arguments...\n"
+					+ "Arguments: <min-example-set-size> <max-example-set-size> <increment> \n"
+					+ "e.g. to train on example sets of size 10, 15, and 20, use arguments '10 20 5' \n"
+					+ "e.g. to train on a single example set of 20 games, use arguments '20 20 1'");
+			return;
+		}
+		
+		Utilities.setParseableOutput(MultiNBMain.PARSEABLE_OUTPUT);
+		Utilities.setPrintPercentiles(MultiNBMain.PRINT_PERCENTILES);
+		
+		int startSize = RUN_FROM_COMMAND_LINE ? Integer.parseInt(args[0]) : START_SIZE;
+		int endSize = RUN_FROM_COMMAND_LINE ? Integer.parseInt(args[1]) : END_SIZE;
+		int increment = RUN_FROM_COMMAND_LINE ? Integer.parseInt(args[2]) : INCREMENT;
+
+		Utilities.printInfoInline(String.format("%n -- Serializing: %s -- %n", serialize ? "true" : "false"));
+		MultiNBHypothesis mnbh = null;
+		for (int x = startSize; x <= endSize; x += increment)
+			mnbh = trainAndTest(x, TEST);
+			
+		if (serialize) 
+			mnbh.serialize();
+		
+		MyDB.close();
+		
+		final long totalEndTime = System.currentTimeMillis();
+		Utilities.printInfo("\n\nTotal execution time (from process running to termination): " 
+					+ Utilities.msToString(totalEndTime - totalStartTime));
 		
 	}
 	
-	private static MultiNBHypothesis trainAndTest(int numGames, boolean testing){
+	
+	// ===========================================================================
+	
+	
+	private static MultiNBHypothesis trainAndTest(int numGames, boolean testing) {
 		Utilities.printInfo("-------------------------------");
 		Utilities.printInfo("------START OF ROUND (" + (numGames) + ")------");
 		Utilities.printInfo("-------------------------------\n");
@@ -116,43 +182,6 @@ public class MultiNBMain {
 		myGameData.close();
 		return myHypothesis;
 	}
-	
-	private static void testDifferentTrainingSetSizes(String[] args) {
-		testDifferentTrainingSetSizes(args, false);
-	}
-	
-	private static void testDifferentTrainingSetSizes(String[] args, boolean serialize) {
-		final long totalStartTime = System.currentTimeMillis();
-		
-		if (RUN_FROM_COMMAND_LINE && args.length != 3){
-			System.out.println("Error in MultiNBMain: expects 3 arguments...\n"
-					+ "Arguments: <min-example-set-size> <max-example-set-size> <increment> \n"
-					+ "e.g. to train on example sets of size 10, 15, and 20, use arguments '10 20 5' \n"
-					+ "e.g. to train on a single example set of 20 games, use arguments '20 20 1'");
-			return;
-		}
-		
-		Utilities.setParseableOutput(MultiNBMain.PARSEABLE_OUTPUT);
-		Utilities.setPrintPercentiles(MultiNBMain.PRINT_PERCENTILES);
-		
-		int startSize = RUN_FROM_COMMAND_LINE ? Integer.parseInt(args[0]) : START_SIZE;
-		int endSize = RUN_FROM_COMMAND_LINE ? Integer.parseInt(args[1]) : END_SIZE;
-		int increment = RUN_FROM_COMMAND_LINE ? Integer.parseInt(args[2]) : INCREMENT;
 
-		Utilities.printInfoInline(String.format("%n -- Serializing: %s -- %n", serialize ? "true" : "false"));
-		MultiNBHypothesis mnbh = null;
-		for (int x = startSize; x <= endSize; x += increment)
-			mnbh = trainAndTest(x, TEST);
-			
-		if (serialize) 
-			mnbh.serialize();
-		
-		MyDB.close();
-		
-		final long totalEndTime = System.currentTimeMillis();
-		Utilities.printInfo("\n\nTotal execution time (from process running to termination): " 
-					+ Utilities.msToString(totalEndTime - totalStartTime));
-		
-	}
 
 }
