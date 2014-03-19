ee8a5061063008982db2078cedd5bfeda39d3324
Vivek Choksi <viv.choksi@gmail.com>
2014-03-19 01:33:16 -0700

    Modified MATLAB game phase plots to use subplots

--- Plotting/game_phase/gamePhasePlotter.m
+++ Plotting/game_phase/gamePhasePlotter.m
@@ -1,27 +1,46 @@
-fid = fopen('kmeans_heuristic_reduced_ternary.csv');
+% fid = fopen('kmeans_heuristic_reduced_ternary.csv');
 
-% fid = fopen('kmeans_heuristic.csv');
+fid = fopen('kmeans_heuristic_ternary.csv');
+SUPER_TITLE_SIZE = 24;
+
+% All plots will be subplots on one figure
+figure;
+hold on;
 
 % ignore the first this many columns in the csv (perhaps if the first
 % column is the game id)
 columnOffset = 1;
-
 tline = fgetl(fid);
 colors = ['k', 'b', 'g', 'r'];
-color = 1;
+subplotIndex = 0;
 
 while ischar(tline)
+    
+    % Create new figure of 4 plots if current figure is full
+    if (subplotIndex == 0 || subplotIndex > 4)
+        subplotIndex = 1;
+        hold off; figure; hold on;
+        % "Super title" for all subplots
+        annotation('textbox', [0 0.9 1 0.1], ...
+            'String', 'Game Phase Assignments', ...
+            'EdgeColor', 'none', ...
+            'HorizontalAlignment', 'center', ...
+            'FontSize', SUPER_TITLE_SIZE);
+    end
+    
     vec = str2num(tline);
     titleAddition = '';
     
     if (columnOffset ~= 0)
         titleAddition = num2str(vec(1:columnOffset));
     end;
-    
-    plotGamePhase(vec(1+columnOffset:end), titleAddition, colors(color));
+
+    plotGamePhase(vec(1+columnOffset:end), titleAddition, colors(color), subplotIndex);
     tline = fgetl(fid);
     color = mod(color, numel(colors)) + 1;
+    subplotIndex = subplotIndex + 1;
 end
+
 
 fclose(fid);
 disp('Type close all to programatically close all figures');
--- Plotting/game_phase/plotGamePhase.m
+++ Plotting/game_phase/plotGamePhase.m
@@ -1,11 +1,13 @@
 %gamePhaseVector: [1 1 1 1 2 2 2 2 2 2 2 0 0 0 0]
 %color: 'r'
 
-function plotGamePhase(gamePhaseVector, titleAddition, color)
-    AXIS_LABEL_SIZE = 14;
-    TITLE_SIZE = 16;
+function plotGamePhase(gamePhaseVector, titleAddition, color, subplotIndex)
+    AXIS_LABEL_SIZE = 16;
+    TITLE_SIZE = 18;
+    
+    % Amount by which to shift plots downward to accomodate for super title.
+    Y_DOWNSHIFT = -0.02; 
 
-    figure;
     if (strcmp(titleAddition, ''))
         titleAddition = 'no args';
     end;
@@ -19,12 +21,24 @@ function plotGamePhase(gamePhaseVector, titleAddition, color)
         % ylim([min(min(gamePhaseVector))-.5 max(max(gamePhaseVector))+.5]);
         legend('Beginning game', 'Middle game', 'End game');
     else
-        plot(moveNums, gamePhaseVector, color);
+        % Plot 4 figures on a single plot
+        handle = subplot(2, 2, subplotIndex);
+        plot(moveNums, gamePhaseVector, color, 'LineSmoothing','on');
         ylim([min(gamePhaseVector)-.5 max(gamePhaseVector)+.5]);
     end
     
     xlabel('Move Number', 'FontSize', AXIS_LABEL_SIZE);
-    ylabel('Game Phase (0 = beginning, 1 = middle, 2 = end)', 'FontSize', AXIS_LABEL_SIZE);
-    title(strcat('Game Phase Performance (Game ID: ', titleAddition, ')'), 'FontSize', TITLE_SIZE);
+    yLabelStrings = {'Beginning', 'Middle', 'End'};
+    set(gca, 'YTickLabel', yLabelStrings, 'YTick', 0:numel(yLabelStrings)-1);
+    set(gca, 'FontSize', AXIS_LABEL_SIZE);    
+    
+    title(strcat('Game ID: ', titleAddition), 'FontSize', TITLE_SIZE);
     set(gcf,'color','w');
+    ct = camtarget;
+    camtarget([ct(1)+0.001*ct(1) ct(2)+0.001 ct(3)]);
+    subplotPos = get(handle,'position');
+    set(handle, 'position', subplotPos + [0 Y_DOWNSHIFT 0 Y_DOWNSHIFT]);
+
+    % ylabel('Game Phase (0 = beginning, 1 = middle, 2 = end)', 'FontSize', AXIS_LABEL_SIZE);
+    % title(strcat('Game Phase Performance (Game ID: ', titleAddition, ')'), 'FontSize', TITLE_SIZE);
 end
